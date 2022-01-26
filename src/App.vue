70b9a49c3704fe719fc6f54a7a6b6fa8e7fc5394
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2022-01-26 17:07:48 +0100

    filer

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div
+      class="bg-purple-400 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0"
+      style="font-family:'Lato',sans-serif;"
+    >
+      <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <section>
+          <h3 class="font-bold text-2xl">Tec bruteforce</h3>
+          <p class="text-gray-600 pt-2">start med hacking.</p>
+        </section>
+
+        <section class="mt-10">
+          <form class="flex flex-col" method="POST" action="#">
+            <div class="mb-6 pt-3 rounded bg-gray-200">
+              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              />
+            </div>
+            <div class="mb-6 pt-3 rounded bg-gray-200">
+              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"
+              />
+            </div>
+            <button
+              class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+              type="submit"
+            >Sign In</button>
+          </form>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
